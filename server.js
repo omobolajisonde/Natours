b0dc5e0adea0e9eb96340dd2401738c54f46707a
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-09-21 14:29:49 +0100

    Integrating MongoDB and Mongoose

--- server.js
+++ server.js
@@ -1,11 +1,21 @@
+const mongoose = require('mongoose');
 const dotenv = require('dotenv');
 
 dotenv.config({ path: './config.env' });
 
 const app = require('./app');
 
-// console.log(app.get('env')); // development
-// console.log(process.env);
+const DB = process.env.DATABASE.replace(
+  '<PASSWORD>',
+  process.env.DATABASE_PASSWORD
+);
+
+async function main() {
+  await mongoose.connect(DB);
+  console.log('DB connection successful!!!');
+}
+
+main().catch((err) => console.log(err));
 
 // APP VARIABLES
 const PORT = process.env.PORT || 8000;
@@ -14,3 +24,5 @@ const HOST = process.env.HOST || '127.0.0.1';
 app.listen(PORT, HOST, () => {
   console.log(`Running on port ${PORT}...`);
 });
+
+// console.log(app.get('env')); // development
