07d5a57ee829e536e78115754d29aeb7bd3723e9
Sonde Omobolaji <wisdomomobolaji@gmail.com>
2022-09-23 19:30:52 +0100

    Implemented all endpoints for GET method

--- controllers/toursControllers.js
+++ controllers/toursControllers.js
@@ -1,27 +1,46 @@
 const Tour = require('../models/toursModel');
 
-exports.getAllTours = function (req, res) {
-  res.status(200).json({
-    success: true,
-    requestedAt: req.requestTime,
-    // results: tours.length,
-    // tours,
-  });
+exports.getAllTours = async function (req, res) {
+  try {
+    const tours = await Tour.find();
+    res.status(200).json({
+      success: true,
+      results: tours.length,
+      data: {
+        tours,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error,
+    });
+  }
 };
 
-exports.getTour = function (req, res) {
-  res.status(200).json({
-    success: true,
-    // tour,
-  });
+exports.getTour = async function (req, res) {
+  try {
+    const tour = await Tour.findById(req.params.id); // Tour.findOne({_id: req.params.id});
+    res.status(200).json({
+      success: true,
+      data: {
+        tour,
+      },
+    });
+  } catch (error) {
+    res.status(404).json({
+      success: false,
+      message: error,
+    });
+  }
 };
 
 exports.createTour = async function (req, res) {
   try {
-    const toBeCreatedTour = new Tour(req.body);
-    const newTour = await toBeCreatedTour.save();
+    // const toBeCreatedTour = new Tour(req.body);
+    // const newTour = await toBeCreatedTour.save();
 
-    // const newTour = await Tour.create(req.body);
+    const newTour = await Tour.create(req.body);
     res.status(201).json({
       success: true,
       data: {
